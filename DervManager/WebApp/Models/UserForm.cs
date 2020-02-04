295e9fde2350b9b14ae3614d0a0ee87b70744f4c
NilTor <zpty@outlook.com>
2020-02-04 23:53:46 +0800

    test blazor form

---
+++ DervManager/WebApp/Models/UserForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class UserForm
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Realname { get; set; }
+    }
+}
